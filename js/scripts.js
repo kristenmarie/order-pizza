5b699128d85257a6eb490cfb1e0627d26930edf6
Kristen Kulha <kristen.m.kulha@gmail.com>
2017-08-25 14:36:31 -0700

    Move address variables into if statment.

--- js/scripts.js
+++ js/scripts.js
@@ -36,11 +36,6 @@ $(document).ready(function(){
     var toppings = [];
     var size = $("select#size").val();
     var cost = 0.00;
-    var firstName = $("input#first-name").val();
-    var lastName = $("input#last-name").val();
-    var street = $("input#street").val();
-    var city = $("input#city").val();
-    var zip = $("input#zip").val();
     var orderType = $('input[name=orderType]:checked').val();
     $("input:checkbox[name=topping]:checked").each(function() {
       toppings.push($(this).val());
@@ -54,6 +49,11 @@ $(document).ready(function(){
     $("#order-confirmation").show();
     $(".form-container").hide();
     if(orderType ==="delivery") {
+      var firstName = $("input#first-name").val();
+      var lastName = $("input#last-name").val();
+      var street = $("input#street").val();
+      var city = $("input#city").val();
+      var zip = $("input#zip").val();
       var customer = new Customer(firstName, lastName, street, city, zip);
       $("#customer-address").append("<h3>Hot pizza is coming at ya!</h3><br><h4>"
       + customer.firstName + " " + customer.lastName + "<br>" + customer.street +
